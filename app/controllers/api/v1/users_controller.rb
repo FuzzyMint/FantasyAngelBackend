0aae8f2c8465c1d0d5e3acc3f78b74668d9b7ba2
Yusuf Sobh <fuzzymintgames@gmail.com>
2013-01-19 15:12:31 -0600

    first commit

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::UsersController  < ApplicationController 
+  before_filter :authenticate_user!
+  respond_to :json
+  
+  def index
+    @users = User.all
+    respond_with(@users)
+  end
+
+end  
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FantasyAngelBackend::Application.config.secret_token = '7fa51dcb6423fc9e159765ebbd7663762e0c2fd8506fb91ee9d3de2a4dabc33061741bec6b61b0d1ddac2f0d8038afce0dc68c330687b13901258a3e2d0dc277'
